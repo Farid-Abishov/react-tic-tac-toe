e76458b1ddc7169d47fc202159276e9e346832aa
unknown <abisovfarid14@gmail.com>
2024-04-26 16:17:45 +0400

    creating components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Player from "./components/Player";
 
 function App() {
-  
-
   return (
-    <h1>React Tic-Tac-Toe</h1>
-  )
+    <main>
+      <div id="game-container">
+        <ol id="players">
+         <Player name="Ali" symbol="X"/>
+         <Player name="Farid" symbol="O"/>
+        </ol>
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
