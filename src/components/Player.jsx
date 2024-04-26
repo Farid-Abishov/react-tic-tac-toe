aa68011a970f322ca78d1c85a2289afd6a081ff4
unknown <abisovfarid14@gmail.com>
2024-04-26 16:54:12 +0400

    adding state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,24 @@
-export default function Player({name,symbol}) {
+import { useState } from "react";
+
+export default function Player({ name, symbol }) {
+  const [isEtiding, SetIsediting] = useState(false);
+
+  function ChangeEdit() {
+    SetIsediting(true);
+  }
+  let playerName = <span className="player-name">{name}</span>;
+
+  if (isEtiding) {
+    playerName = <input type="text" required />;
+  }
+
   return (
     <li>
-      <span className="player-name">{name}</span>
-      <span className="player-name">{symbol}</span>
-      <button>Edit</button>
+      <span className="player">
+        {playerName}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={ChangeEdit}>Edit</button>
     </li>
   );
 }
